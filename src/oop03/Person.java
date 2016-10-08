31d1fd66c8bc23a680dafcc6c028aa776317c05a
yuge1 <1365692736@qq.com>
2016-10-08 19:00:56 +0800

    本地上传

---
+++ src/oop03/Person.java
@@ -0,0 +1,17 @@
+package oop03;
+
+public class Person {
+	String name;
+	int age;
+	String XingBie;
+	String Weight;
+	//行为
+	void chi(){
+		System.out.println("我喜欢吃翔！");
+		
+		}void aiHao(){
+			System.out.println("我是要成为游戏王的男人！");
+	}void jieShao(){System.out.println("大家好，我叫"+name+"性别:"+XingBie+"年龄："+age+"体重是："+Weight);
+		
+	}
+}
---
+++ src/oop03/TestPerson.java
@@ -0,0 +1,17 @@
+package oop03;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person d= new Person();
+		d.name ="ÂÞÐ¡Ø©";
+		d.age = 20;
+		d.Weight = "45¹«½ï";
+		d.XingBie="ÄÐ";
+		d.jieShao();
+		d.chi();
+		d.aiHao();
+	}
+
+}
